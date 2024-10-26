e3076f4e647e1c6ffab2f3e1f2b0c6eab7fba10f
ba144220 <b08901097@ntu.edu.tw>
2024-10-26 13:04:37 +0800

    cache

--- src/data/load_table_datasets.py
+++ src/data/load_table_datasets.py
@@ -1,44 +1,134 @@
 import os
 import re
 import datasets
-from typing import Dict
+from transformers import PreTrainedTokenizerFast
 
-def load_table_datasets(
-    dataset_path: str,
-    table_extension: str = "html",
-    shuffle: bool = False,
-    test_max_samples: int = None,
-    val_max_samples: int = None,
-    train_max_samples: int = None
-):
-  
-    dataset = datasets.load_dataset("csv", data_files={
-        "train": os.path.join(dataset_path, "data", "train.csv"),
-        "test": os.path.join(dataset_path, "data", "test.csv"),
-        "validation": os.path.join(dataset_path, "data", "val.csv")
-    })
+SYSTEM_PROMPT = "You are a helpful assistant that can answer questions about the table."
+ASSISTANT_PROMPT = "Answer: "
+
+class TableDataset():
+    def __init__(
+        self, 
+        dataset_path: str,
+        tokenizer: PreTrainedTokenizerFast,
+        table_extension: str = "html",
+        shuffle: bool = False,
+        test_max_samples: int = None,
+        val_max_samples: int = None,
+        train_max_samples: int = None,
+        system_prompt: str = SYSTEM_PROMPT,
+        assistant_prompt: str = ASSISTANT_PROMPT
+    ):
+        
+        self.dataset_path = dataset_path
+        self.dataset = datasets.load_dataset("csv", data_files={
+            "train": os.path.join(dataset_path, "data", "train.csv"),
+            "test": os.path.join(dataset_path, "data", "test.csv"),
+            "validation": os.path.join(dataset_path, "data", "val.csv")
+        })
+        if shuffle:
+            self.dataset = self.dataset.shuffle(seed=42)
+        if test_max_samples is not None:
+            self.dataset["test"] = self.dataset["test"].select(range(test_max_samples))
+        if val_max_samples is not None:
+            self.dataset["validation"] = self.dataset["validation"].select(range(val_max_samples))
+        if train_max_samples is not None:
+            self.dataset["train"] = self.dataset["train"].select(range(train_max_samples))
+            
+        self.tokenizer = tokenizer
+        self.table_extension = table_extension
+        self.system_prompt = system_prompt
+        self.assistant_prompt = assistant_prompt
+        
+        self.dataset = self.dataset.map(self._preprocess_single_example_to_string, batched=False)
     
-    if shuffle:
-        dataset = dataset.shuffle(seed=42)
+    def get_dataset(self):
+        return self.dataset
     
-    if test_max_samples is not None:
-        dataset["test"] = dataset["test"].select(range(test_max_samples))
-    if val_max_samples is not None:
-        dataset["validation"] = dataset["validation"].select(range(val_max_samples))
-    if train_max_samples is not None:
-        dataset["train"] = dataset["train"].select(range(train_max_samples))
-    
-    def preprocess_function(examples):
-        # Get the `context` column
-        context = examples["context"]
-        # Load the table file as string
-        # Remove the extension from the context
+    def _get_table(self, context: str):
         context = re.sub(f".csv$", "", context)
-        with open(os.path.join(dataset_path, context + "." + table_extension), "r", encoding="utf-8") as f:
-            table = f.read()
-        examples["table"] = table
-        return examples
-
-    dataset = dataset.map(preprocess_function, batched=False)
+        with open(os.path.join(self.dataset_path, context + "." + self.table_extension), "r", encoding="utf-8") as f:
+            return f.read()
+    def _preprocess_single_example_to_string(self, example):
+        
+        table = self._get_table(example["context"])
+        example["table"] = table
+        
+        user_prompt = example["question"] + "\n" + table
+        messages = [
+            {"role": "system", "content": self.system_prompt},
+            {"role": "user", "content": user_prompt}
+        ]
+        
+        text = self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+        text = text + self.assistant_prompt
+        example["text"] = text
+        return example
+                
+       
     
-    return dataset
+
+# def load_table_datasets(
+#     dataset_path: str,
+#     tokenizer: PreTrainedTokenizerFast,
+#     table_extension: str = "html",
+#     shuffle: bool = False,
+#     test_max_samples: int = None,
+#     val_max_samples: int = None,
+#     train_max_samples: int = None
+# ):
+  
+#     dataset = datasets.load_dataset("csv", data_files={
+#         "train": os.path.join(dataset_path, "data", "train.csv"),
+#         "test": os.path.join(dataset_path, "data", "test.csv"),
+#         "validation": os.path.join(dataset_path, "data", "val.csv")
+#     })
+    
+#     if shuffle:
+#         dataset = dataset.shuffle(seed=42)
+    
+#     if test_max_samples is not None:
+#         dataset["test"] = dataset["test"].select(range(test_max_samples))
+#     if val_max_samples is not None:
+#         dataset["validation"] = dataset["validation"].select(range(val_max_samples))
+#     if train_max_samples is not None:
+#         dataset["train"] = dataset["train"].select(range(train_max_samples))
+    
+#     def preprocess_function(examples, tokenizer: PreTrainedTokenizerFast):
+#         # Get the `context` column
+#         context = examples["context"]
+#         # Load the table file as string
+#         # Remove the extension from the context
+#         context = re.sub(f".csv$", "", context)
+#         with open(os.path.join(dataset_path, context + "." + table_extension), "r", encoding="utf-8") as f:
+#             table = f.read()
+#         examples["table"] = table
+#         # Combine 'question' and 'context' if necessary
+#         inputs = [q + ' ' + c for q, c in zip(examples['question'], examples['table'])]
+        
+#         # Tokenize the inputs
+#         model_inputs = tokenizer(
+#             inputs, 
+#             max_length=512, 
+#             truncation=True, 
+#             padding='max_length'  # or 'longest' or False
+#         )
+        
+#         # Tokenize labels if you have them (e.g., 'answer')
+#         if 'answer' in examples:
+#             with tokenizer.as_target_tokenizer():
+#                 labels = tokenizer(
+#                     examples['answer'], 
+#                     max_length=512, 
+#                     truncation=True, 
+#                     padding='max_length'
+#                 )
+#             model_inputs['labels'] = labels['input_ids']
+        
+#         return model_inputs
+
+    
+#     dataset = dataset.map(preprocess_function, batched=False, fn_kwargs={"tokenizer": tokenizer})
+
+#     return dataset
+
--- src/run_evaluation.py
+++ src/run_evaluation.py
@@ -1,21 +1,85 @@
 import datasets
 import re
 from trl import SFTTrainer, SFTConfig
-from transformers import LlamaForCausalLM, LlamaTokenizerFast
-from data.load_table_datasets import load_table_datasets
+from transformers import (
+    LlamaForCausalLM, 
+    PreTrainedTokenizerFast, 
+    BitsAndBytesConfig, 
+    TrainerCallback
+)
+from peft import LoraConfig
+from data.load_table_datasets import TableDataset
 MODEL_NAME = "meta-llama/Llama-3.2-1B-Instruct"
 DATASET_NAME = "self_generated"
-TABLE_EXTENSION = "html"
+TABLE_EXTENSION = "csv"
 
 
 def main():
-    dataset = load_table_datasets("datasets/self_generated", TABLE_EXTENSION, shuffle=True, test_max_samples=100, val_max_samples=100, train_max_samples=100)
+    tokenizer = PreTrainedTokenizerFast.from_pretrained("meta-llama/Llama-3.2-1B-Instruct")
+    dataset = TableDataset(
+        dataset_path="datasets/self_generated",
+        tokenizer=tokenizer,
+        table_extension=TABLE_EXTENSION,
+        train_max_samples=100,
+        val_max_samples=100,
+        test_max_samples=100
+    ).get_dataset()
     print(dataset)
-    # print(dataset["train"][0])
-    # print(dataset["train"][0]["table"])
+    print(dataset["train"])
+    print(dataset["train"][0]["text"])
+    
+    # bnb_config = BitsAndBytesConfig(
+    #     load_in_8bit=True,
+    # )
 
-    # model = LlamaForCausalLM.from_pretrained("meta-llama/Llama-3.2-1B-Instruct")
-    # tokenizer = LlamaTokenizerFast.from_pretrained("meta-llama/Llama-3.2-1B-Instruct")
+    # model = LlamaForCausalLM.from_pretrained("meta-llama/Llama-3.2-1B-Instruct", quantization_config=bnb_config, low_cpu_mem_usage=True)
+    # # Set the padding side to left
+    # tokenizer.padding_side = "left"
+    # tokenizer.pad_token = tokenizer.eos_token
+    
+    # dataset = load_table_datasets(
+    #     "datasets/self_generated", 
+    #     tokenizer,
+    #     TABLE_EXTENSION, 
+    #     shuffle=True, 
+    #     test_max_samples=100, 
+    #     val_max_samples=100, 
+    #     train_max_samples=100
+    # )
+    # print(dataset)
+    # print(dataset["train"][0])
+    # print(dataset["train"][0]["input_ids"])
+    
+
+    # peft_config = LoraConfig(
+    #     r=16,
+    #     lora_alpha=32,
+    #     lora_dropout=0.05,
+    #     bias="none",
+    #     task_type="CAUSAL_LM",
+    # )
+    
+   
+    
+    # trainer = SFTTrainer(
+    #     model=model,
+    #     tokenizer=tokenizer,
+    #     train_dataset=dataset["train"],
+    #     eval_dataset=dataset["validation"],
+    #     peft_config=peft_config,
+    #     args=SFTConfig(
+    #         max_seq_length=2048,
+    #         dataset_text_field="question",
+    #         output_dir="../outputs",
+    #         remove_unused_columns=False,
+    #     ),
+       
+    # )
+    
+    # print(model.config)
+
+    # results = trainer.predict(dataset["test"])
+    # print(results)
 
 
 if __name__ == "__main__":
