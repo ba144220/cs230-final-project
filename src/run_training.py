95ef0c1431b13005c69172dfc5b380b6a0544e25
ba144220 <b08901097@ntu.edu.tw>
2024-11-08 14:06:45 +0800

    fix: parser and sft_config

--- src/run_training.py
+++ src/run_training.py
@@ -1,16 +1,18 @@
+import sys
+
 from transformers import (
     LlamaForCausalLM, 
     PreTrainedTokenizerFast, 
     BitsAndBytesConfig,
     HfArgumentParser,
-    TrainingArguments
 )
 
 from trl import (
-    SFTTrainer
+    SFTTrainer,
+    SFTConfig
 )
 
-from peft import LoraConfig, get_peft_model
+from peft import LoraConfig
 from data.load_table_datasets import load_table_datasets
 from args.args_class import (
     TrainArguments,
@@ -22,14 +24,13 @@ def main():
     ################
     # ArgumentParser
     ################
-    peft_parser = HfArgumentParser(PeftArguments)
-    peft_args = peft_parser.parse_args_into_dataclasses()[0]
-
-    train_parser = HfArgumentParser(TrainArguments)
-    train_args = train_parser.parse_args_into_dataclasses()[0]
-
-    data_parser = HfArgumentParser(DataArguments)
-    data_args = data_parser.parse_args_into_dataclasses()[0]
+    # Add json option
+    
+    parser = HfArgumentParser(PeftArguments, TrainArguments, DataArguments)
+    if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
+        peft_args, train_args, data_args = parser.parse_json_file(json_file=sys.argv[1])
+    else:
+        peft_args, train_args, data_args = parser.parse_args_into_dataclasses()
 
     ################
     # Model init & Tokenizer
@@ -61,32 +62,33 @@ def main():
     ################
     # Training
     ################
+    model.generation_config.pad_token_id = tokenizer.pad_token_id
+    
     peft_config = LoraConfig(
-    r=peft_args.r,
-    lora_alpha=peft_args.lora_alpha,
-    lora_dropout=peft_args.lora_dropout,
-    bias=peft_args.bias,
-    task_type=peft_args.task_type,
+        r=peft_args.r,
+        lora_alpha=peft_args.lora_alpha,
+        lora_dropout=peft_args.lora_dropout,
+        bias=peft_args.bias,
+        task_type=peft_args.task_type,
     )
+    
 
-    peft_model = get_peft_model(model, peft_config)
-    peft_model.generation_config.pad_token_id = tokenizer.pad_token_id
-
-    training_args = TrainingArguments(
+    training_args = SFTConfig(
         per_device_train_batch_size=train_args.per_device_train_batch_size,
         num_train_epochs=train_args.num_train_epochs,
         learning_rate=train_args.learning_rate,
         bf16=True,
         save_total_limit=train_args.save_total_limit,
         logging_steps=train_args.logging_steps,
-        output_dir=train_args.output_dir
+        output_dir=train_args.output_dir,
     )
     trainer = SFTTrainer(
-        peft_model,
+        model,
         train_dataset=dataset['train'],
         tokenizer=tokenizer,
         max_seq_length=train_args.max_seq_length,
-        args=training_args
+        args=training_args,
+        peft_config=peft_config
     )
     trainer.train() 
     trainer.save_model(train_args.output_dir)
