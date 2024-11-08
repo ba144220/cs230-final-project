9bd159972bc2cc03aeab6112ea3a90560a762e53
YejuAhn <yeju12331@gmail.com>
2024-11-08 17:36:31 +0800

    experiment: add training json with bash script with output dir with run number

--- src/run_training.py
+++ src/run_training.py
@@ -1,4 +1,6 @@
 import sys
+import json
+from datetime import datetime
 
 from transformers import (
     LlamaForCausalLM, 
@@ -20,17 +22,38 @@ from args.args_class import (
     DataArguments
 )
 
+def track_run_number():
+    try:
+        with open("src/args/run_number.txt", "r") as f:
+            run_number = int(f.read()) + 1
+    except FileNotFoundError:
+        run_number = 1
+
+    with open("src/args/run_number.txt", "w") as f:
+        f.write(str(run_number))
+    return run_number
+
+def get_run_file_name():
+    run_number = track_run_number()
+    now = datetime.now()
+    current_time = now.strftime("%H%M%S")
+    return 'run_{0}_{1}'.format(run_number, current_time)
+
+def save_training_args_to_json(training_args, output_dir, run_file_name):  
+    with open('{0}/{1}_training.json'.format(output_dir, run_file_name), 'w') as fout:
+        fout.write(training_args.to_json_string())
+
 def main():
     ################
     # ArgumentParser
     ################
-    # Add json option
-    
     parser = HfArgumentParser([PeftArguments, TrainArguments, DataArguments])
     if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
         peft_args, train_args, data_args = parser.parse_json_file(json_file=sys.argv[1])
     else:
         peft_args, train_args, data_args = parser.parse_args_into_dataclasses()
+    run_file_name = get_run_file_name()
+    output_run_file_name_dir = '{0}/{1}'.format(data_args.output_dir, run_file_name)
 
     ################
     # Model init & Tokenizer
@@ -80,7 +103,7 @@ def main():
         bf16=True,
         save_total_limit=train_args.save_total_limit,
         logging_steps=train_args.logging_steps,
-        output_dir=data_args.output_dir,
+        output_dir=output_run_file_name_dir,
     )
     trainer = SFTTrainer(
         model,
@@ -90,9 +113,10 @@ def main():
         args=training_args,
         peft_config=peft_config
     )
-    trainer.train() 
-    trainer.save_model(data_args.output_dir)
+    trainer.train()
+    trainer.save_model(output_run_file_name_dir)
+    save_training_args_to_json(training_args, output_run_file_name_dir, run_file_name)
     print("Model trained successfully.")
-   
+
 if __name__ == "__main__":
     main()
---
+++ src/scripts/run_training.sh
@@ -0,0 +1,1 @@
+python src/run_training.py src/args/training_args.json
